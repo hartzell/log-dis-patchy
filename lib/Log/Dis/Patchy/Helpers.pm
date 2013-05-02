581e931b409942cc11903a6ec8b05a2b413aca25
George Hartzell <hartzell@alerce.com>
2013-05-02 12:03:34 -0700

    Add timestamp_prefix_callback helper.

--- lib/Log/Dis/Patchy/Helpers.pm
+++ lib/Log/Dis/Patchy/Helpers.pm
@@ -3,8 +3,9 @@ package Log::Dis::Patchy::Helpers;
 
 use Sub::Exporter -setup => {
     exports => [
-        qw(add_newline_callback
-            log_pid_callback)
+        qw( add_newline_callback
+            log_pid_callback
+            timestamp_prefix_callback )
     ],
 };
 
@@ -20,6 +21,13 @@ sub add_newline_callback {
     return sub {
         my %i = @_;
         return $i{message} . "\n";
+    };
+}
+
+sub timestamp_prefix_callback {
+    return sub {
+        my %i = @_;
+        return (localtime) . ' ' . $i{message};
     };
 }
 
